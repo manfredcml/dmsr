f289253a8b34c70bac8ea53cff0d27867298e4c1
Manfred Lee <manfredcml@gmail.com>
2023-02-23 17:52:44 +0800

    Refactor main

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,49 +1,47 @@
 mod args;
 
-use std::thread;
+use anyhow::Result;
 use args::Args;
 use clap::Parser;
-use log::{error, info};
+use dms::events::event_stream::DataStream;
 use dms::sources::config::Config;
 use dms::sources::postgres::PostgresSource;
 use dms::sources::source::Source;
-use anyhow::Result;
-use dms::events::event_stream::DataStream;
-use futures::executor::block_on;
+use log::{error, info};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  env_logger::init();
+    env_logger::init();
 
-  info!("Starting up...");
+    info!("Starting up...");
 
-  let args = Args::parse();
-  let source = args.source.as_str();
+    let args = Args::parse();
+    let source = args.source.as_str();
 
-  match source {
-    "postgres" => {
-      let mut source = PostgresSource::new(Config {
+    match source {
+        "postgres" => {
+            let mut source = PostgresSource::new(Config {
         endpoint: "host=localhost user=postgres password=postgres dbname=postgres replication=database".to_string(),
       });
-      source.connect().await?;
+            source.connect().await?;
 
-      let mut event_stream = DataStream::new();
+            let mut event_stream = DataStream::new();
 
-      let tx_future = source.stream(&mut event_stream.tx);
-      let rx_future = async {
-        loop {
-          let event = event_stream.rx.recv().await;
-          println!("Here you go - {:?}", event);
+            let tx_future = source.stream(&mut event_stream.tx);
+            let rx_future = async {
+                loop {
+                    let event = event_stream.rx.recv().await;
+                    println!("Here you go - {:?}", event);
+                }
+            };
+
+            let (_, _) = tokio::join!(tx_future, rx_future);
         }
-      };
-
-      let (_, _) = tokio::join!(tx_future, rx_future);
+        _ => {
+            error!("Unknown source: {}", source);
+        }
     }
-    _ => {
-      error!("Unknown source: {}", source);
-    }
-  }
 
-  info!("Terminating...");
-  Ok(())
+    info!("Terminating...");
+    Ok(())
 }
